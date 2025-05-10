e55d39f4e1a7aa8b29a78872df0a98aa9c9232dc
grizhlieCodes <rafal.potasz@gmail.com>
2025-05-10 08:51:43 +0100

    created components

---
+++ src/lib/components/layout/Container.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { cn } from '$lib/scripts/utils';
+
+	let { children, class: className }: { children?: any; class?: string } = $props();
+
+	let BASE_STYLING = `lg:gap-12 flex w-full max-w-[71.25rem] flex-col items-center justify-center
+    gap-8 text-center md:gap-10`;
+	let styles = $derived(cn(BASE_STYLING, className));
+</script>
+
+<div class={styles}>
+	{#if children}
+		{@render children?.()}
+	{/if}
+</div>
---
+++ src/lib/components/ui/BasicCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/scripts/utils';
+
+	let { children, class: className }: { children: any; class?: string } = $props();
+
+	let CARD_BASE_STYLING = `bg-primary-50 dark:bg-primary-950 border-primary-200 dark:border-primary-700 flex flex-col
+                items-center gap-3 border w-full py-4 px-8 rounded-xl max-w-xs sm:max-w-none`;
+
+	let styles = $derived(cn(CARD_BASE_STYLING, className));
+</script>
+
+<div class={styles}>
+	{#if children}
+		{@render children?.()}
+	{/if}
+</div>
