39ae03ce734babba8aeb0b2081df7daac1659cc7
grizhlieCodes <rafal.potasz@gmail.com>
2025-05-12 16:01:37 +0100

    updated home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
-	import Home from '$lib/pages/Home.svelte';
+	import HomeFaq from '$lib/pages/home/HomeFaq.svelte';
+	import HomeHero from '$lib/pages/home/HomeHero.svelte';
+	import HomeMyApproach from '$lib/pages/home/HomeMyApproach.svelte';
+	import HomeMyServices from '$lib/pages/home/HomeMyServices.svelte';
+	import HomepageProblem from '$lib/pages/home/HomepageProblem.svelte';
+	import HomepageSolution from '$lib/pages/home/HomepageSolution.svelte';
+	import HomeTestimonial from '$lib/pages/home/HomeTestimonial.svelte';
+	import PageCta from '$lib/pages/shared/PageCta.svelte';
 </script>
 
-<Home></Home>
+<HomeHero></HomeHero>
+<HomeTestimonial></HomeTestimonial>
+<HomepageProblem></HomepageProblem>
+<HomepageSolution></HomepageSolution>
+<HomeMyApproach></HomeMyApproach>
+<HomeMyServices></HomeMyServices>
+<HomeFaq></HomeFaq>
+<PageCta></PageCta>
