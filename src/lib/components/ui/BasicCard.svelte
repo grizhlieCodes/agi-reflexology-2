5b1db2811aa3b11e9a23b41e46e1d0f5fa1c9b43
grizhlieCodes <rafal.potasz@gmail.com>
2025-05-15 08:54:34 +0100

    additions to basic-card

--- src/lib/components/ui/BasicCard.svelte
+++ src/lib/components/ui/BasicCard.svelte
@@ -2,25 +2,28 @@
 	import { cn } from '$lib/scripts/utils';
 
 	type Props = {
+		element: 'div' | 'li' | string;
 		children: any;
 		class?: string;
 		type?: 'primary' | 'neutral';
+		includeMaxWidthStyling?: boolean;
 	};
 
-	let { children, class: className, type = 'primary' }: Props = $props();
+	let { children, class: className, type = 'primary', element = 'div', includeMaxWidthStyling = true }: Props = $props();
 
 	let COLOR_STYLES = {
 		primary: `bg-primary-50 dark:bg-primary-950 border-primary-200 dark:border-primary-700`,
 		neutral: `bg-neutral-50 dark:bg-neutral-950 border-neutral-200 dark:border-neutral-700`
 	};
 
-	let CARD_BASE_STYLING = ` flex flex-col items-center gap-3 border w-full py-4 px-8 rounded-xl max-w-xs sm:max-w-none`;
+	let CARD_BASE_STYLING = ` flex flex-col items-center gap-3 border w-full py-4 px-8 rounded-xl`;
+	let MAX_W_STYLING = ` max-w-xs sm:max-w-none`;
 
-	let styles = $derived(cn(CARD_BASE_STYLING, COLOR_STYLES[type], className));
+	let styles = $derived(cn(CARD_BASE_STYLING, COLOR_STYLES[type], includeMaxWidthStyling ? MAX_W_STYLING : '', className));
 </script>
 
-<div class={styles}>
+<svelte:element this={element} class={styles}>
 	{#if children}
 		{@render children?.()}
 	{/if}
-</div>
+</svelte:element>
